3df17b7c674a64603d9f56cbb5125d6c9e8b8c71
P <balan@cse.iitb.ac.in>
2021-09-23 22:09:13 +0530

    updated

--- train_code.py
+++ train_code.py
@@ -16,36 +16,34 @@ class train_model(tf.keras.Model):
         x = self.resnet(x)
         return self.arcface(x, y)
 # Instantiate a loss function.
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+def loss_fxn(logits,labels):
+    loss_fn = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    return loss_fn
 # Instantiate an optimizer to train the model.
 optimizer = tf.keras.optimizers.Adam(lr=hypar.learning_rate)
-
-# Prepare the metrics.
-train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
 
 model = train_model()
 
 @tf.function
 def train_step(images, labels):
     with tf.GradientTape() as tape:
-        logits = model(tf.slice(images, [0, 0, 0, 0], [hypar.batch_size, 112, 112, 3]), tf.slice(labels, [0], [hypar.batch_size]))
-        for i in range(hypar.batch_multiplier - 1):
-            logits = tf.concat([logits, model(tf.slice(images, [hypar.batch_size * (i + 1), 0, 0, 0], [hypar.batch_size, 112, 112, 3]), tf.slice(labels, [hypar.batch_size * (i + 1)], [hypar.batch_size]))], 0)
+        logits = model(images,labels)
         pred = tf.nn.softmax(logits)
-        inf_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+        inf_loss = loss_fxn(logits,labels)
         loss = inf_loss
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    train_acc_metric.update_state(labels, logits)
     train_loss = tf.reduce_mean(loss)
-    accuracy = train_acc_metric.result()
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred, axis=1, output_type=tf.dtypes.int32), labels), dtype=tf.float32))
     inference_loss = tf.reduce_mean(inf_loss)
-
-    return accuracy, train_loss, inference_loss
+    regularization_loss = 0
+    return accuracy, train_loss, inference_loss, regularization_loss
 
 images = load('x_train.npy')
 labels = load('y_train.npy')
+# Prepare the training dataset.
+train_dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(hypar.batch_size)
 
 epochs = 2
 for epoch in range(epochs):
@@ -54,19 +52,15 @@ for epoch in range(epochs):
 
     # Iterate over the batches of the dataset.
     for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
-        loss_value = train_step(x_batch_train, y_batch_train)
-
+        accuracy, train_loss, inference_loss, regularization_loss = train_step(x_batch_train, x_batch_train, hypar.reg_coef)
         # Log every 200 batches.
         if step % 200 == 0:
             print(
                 "Training loss (for one batch) at step %d: %.4f"
-                % (step, float(loss_value))
+                % (step, float(train_loss))
             )
-            print("Seen so far: %d samples" % ((step + 1) * batch_size))
+            print("Seen so far: %d samples" % ((step + 1) * hypar.batch_size))
 
-    print("Training acc over epoch: %.4f" % (float(train_acc),))
-
-    # Reset training metrics at the end of each epoch
-    train_acc_metric.reset_states()
+    print("Training acc over epoch: %.4f" % (float(accuracy),))
 
     print("Time taken: %.2fs" % (time.time() - start_time))

48a75e3b81cadf54f3ff7ddb7111f39f8e6cac6d
P <balan@cse.iitb.ac.in>
2021-09-30 22:20:21 +0530

    update

---
+++ recognition_test.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+import math
+import time
+import numpy as np
+
+import hypar
+import arcface
+import network as net
+
+threshold=0.5
+
+class train_model(tf.keras.Model):
+    def __init__(self):
+        super(train_model, self).__init__()
+        #self.resnet = net.Resnet_nn()
+        self.resnet = net.Resnet()
+        self.arcface = arcface.Arcface_Layer()
+
+    def call(self, x, y):
+        x = self.resnet(x)
+        return self.arcface(x, y)
+
+model = train_model()        
+model.load_weights(file_name + '_full_model' + str(file_number)+ '.h5')
+
+def get_embeddings(input_imgs,model):
+    test_model = model.resnet
+    embeddings = test_model.predict(input_imgs)
+    print(embeddings.shape)
+    return embeddings
+
+def get_distance(emb1,emb2):
+    """
+    emb1 & emb2: are both 512 dimensional vectors from the trained resnet model
+
+    get_distance: returns dot_prod,cosine_distance,euclidean_distance
+    """
+    dot_prod = np.dot(emb1,emb2.T)
+
+    a = np.matmul(np.transpose(emb1), emb2)
+    b = np.sum(np.multiply(emb1, emb1))
+    c = np.sum(np.multiply(emb2, emb2))
+    cosine_distance = 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+
+    euclidean_distance = emb1 - emb2
+    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
+    euclidean_distance = np.sqrt(euclidean_distance)
+
+    return dot_prod,cosine_distance,euclidean_distance
+
+def recognise(img,face_imgs_database,face_names_database):
+    """
+    img: numpy array of dims (112,112,3) of image of face to be recognised
+    face_imgs_database: numpy array of dims (num_of_faces,112,112,3)
+    face_names_database: dictionary of form {index_of_face:"name_of_person"} comprising of names of faces in face_imgs_database array
+    
+    recognise: returns the name and face of the closest match in the database
+    """
+    img_embedding = get_embeddings(img)
+    face_embedding_database = get_embeddings(face_imgs_database)
+    best_match_index = []
+    best_distance = float('-inf')
+    for i in range(int(face_embedding_database.shape[0])):
+        distance,cosine_distance,euclidean_distance = get_distance(img_embedding,face_embedding_database[i])
+        if distance>best_distance:
+            best_distance = distance
+            best_match_index = [i]
+        elif distance==best_distance:
+            best_match_index.append(i)
+
+    names = []
+    faces = []
+    for i in best_match_index:
+        names.append(face_names_database[i])
+        faces.append(face_imgs_database[i])
+    
+    return names,faces
+
+def verify(img,face):
+    """
+    img: numpy array of dims (112,112,3) of image of face to be verified
+    face: image of face of person to be verified with
+    
+    verify: returns boolean if the faces match or not
+    """
+    img_embedding = get_embeddings(img)
+    face_embedding = get_embeddings(face)
+    distance,cosine_distance,euclidean_distance = get_distance(img_embedding,face_embedding)
+    if distance>threshold:
+        return True
+    return False

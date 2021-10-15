9a2c3fd7f06bdad754c2daf37f4ab06bacf927c3
P <81023255+zestyoreo@users.noreply.github.com>
2021-10-15 07:52:59 +0530

    umic members preprocessing over

--- umic_database_pre-processing.py
+++ umic_database_pre-processing.py
@@ -1,34 +1,40 @@
 import numpy as np
 import glob
+import random as rnd
+import pickle
+import matplotlib.pyplot as plt
 from numpy import asarray
 from numpy import save
 import cv2
 import os
 
-#names = ["Amit", "atharva", "Balasubramanian", "Harshil", "Meghna", "Rihan Aaron", "Ronit", "Saad", "shubham"]
-names = ["Ronit"]
+names = ["Amit", "atharva", "Balasubramanian", "Harshil", "Meghna", "Rihan Aaron", "Ronit", "Saad", "shubham","Siddhant"]
 down_size = 112
 down_points = (down_size, down_size)
 
-face_cascade = cv2.CascadeClassifier( os.path.join(cv2.data.haarcascades, "haarcascade_frontalface_default.xml") )
-face_database = {}
+face_database = []
 face_names_database = []
 
 for name in names:
-    file_name = "UMIC Members/"+name+"/*.jpeg"
+    file_name = "UMIC Members/"+name+"/*.jpg"
     face_shots = []
     for file in glob.glob(file_name):
         photo = cv2.imread(file)
-        gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        face_img = cv2.resize(photo, down_points, interpolation= cv2.INTER_LINEAR)
+        print(face_img.shape)
+        cv2.imshow("meow", face_img)
+        cv2.waitKey()
+        face_database.append(face_img)
+        face_names_database.append(name)
 
-        for (x, y, w, h) in faces:
-            frame = cv2.rectangle(photo, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            face_img = frame[y:y + h, x:x + w]
-            face_img = cv2.resize(face_img, down_points, interpolation= cv2.INTER_LINEAR)
-            face_img = asarray(face_img)
-            print(face_img.shape)
+database = np.array(face_database[0],dtype=float)
+for i in range(1,len(face_database)):
+    j = np.array(face_database[i],dtype=float)
+    database = np.concatenate((database,j),axis=0)
 
+save("umic_members_images.npy",database)
+file_name = "umic_members_names.pkl"
 
-
-cv2.destroyAllWindows()
+open_file = open(file_name, "wb")
+pickle.dump(face_names_database, open_file)
+open_file.close()

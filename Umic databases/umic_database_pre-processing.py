46f2a71bfc02e622e0d907433e4018e02626fc89
P <81023255+zestyoreo@users.noreply.github.com>
2021-10-15 22:28:47 +0530

    plots and preprocessing

--- Umic databases/umic_database_pre-processing.py
+++ Umic databases/umic_database_pre-processing.py
@@ -8,13 +8,16 @@ from numpy import save
 import cv2
 import os
 
+num_photos = 100
 names = ["Amit", "atharva", "Balasubramanian", "Harshil", "Meghna", "Rihan Aaron", "Ronit", "Saad", "shubham","Siddhant"]
 down_size = 112
 down_points = (down_size, down_size)
 
 face_database = []
 face_names_database = []
-Y = []
+Y = [None] * num_photos
+i = 0
+j = 0
 for name in names:
     file_name = "UMIC Members/"+name+"/*.jpg"
     face_shots = []
@@ -26,14 +29,19 @@ for name in names:
         cv2.waitKey()
         face_database.append(face_img)
         face_names_database.append(name)
-        Y.append(names.index(name))
+        Y[i] = j
+        i+=1
+    j+=1
 
 Y = np.array(Y)
+print(Y.shape)
+
 database = np.array(face_database[0],dtype=float)
 for i in range(1,len(face_database)):
     j = np.array(face_database[i],dtype=float)
     database = np.concatenate((database,j),axis=0)
 
+print(database.shape)
 save("umic_members_images.npy",database)
 save("umic_members_Y.npy",Y)
 file_name = "umic_members_names.pkl"
